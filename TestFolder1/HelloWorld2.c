4b62fa19c22b891daa9889fca068364e2e95725f
LESAULNIER Jerome <jlesaulnier@astek.fr>
2014-06-19 18:37:05 +0200

    Test

--- TestFolder1/HelloWorld2.c
+++ TestFolder1/HelloWorld2.c
@@ -2,7 +2,7 @@
 
 main()
 {
-    cout << "Hello World 2 modified for all PTR !";
+    cout << "Hello World 2 modified for all PTR  !";
     cout << "Test hook pre and post commit 4";
     return 0;
 }
